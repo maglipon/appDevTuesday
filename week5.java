1c0506bd3d01d5081921f20523fbcdc5d63e0aef
marllord <mrregoroso@paterostechnologicalcollege.edu.ph>
2025-03-04 20:03:43 +0800

    Added regoroso.java with age calculation and updated week5.java

--- week5.java
+++ week5.java
@@ -1,45 +1,10 @@
 public class week5 {
     public static void main(String[] args) {
         System.out.println("Collaborative Java Project\n");
-
        
-        alabado.display();
-
-
         // Call each student's method
-
         // More students will add their method calls here...
-        pianar.display();
-
-
-
-        Amar.display();
-
-        ECHON.display(); // Add your method call here
-
-
-       
-        Pajanustan.display();
-
-
-       
-        Serrano.display();
-
-
-        Panis.display(); 
-        Tabon.display();
-        Artiaga.display();
-
-        Balais.display();
-
-
-        Bendoy.display();
-
-
-
-
-
-
-
+        //lastname.display();
+        //Regoroso.display();
     }
 }
