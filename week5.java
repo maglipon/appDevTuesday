1391427d5751fd9bf681019336f7a81c358d5d79
StefannyTapia <sdtapia@paterostechnologicalcollege.edu.ph>
2025-03-04 20:58:25 +0800

    Added Tapia.java with age calculation and updated week5.java

--- week5.java
+++ week5.java
@@ -1,9 +1,7 @@
 public class week5 {
     public static void main(String[] args) {
         System.out.println("Collaborative Java Project\n");
-       
-        // Call each student's method
-        // More students will add their method calls here...
-        //lastname.display();
+
+        Tapia.display();
     }
 }
