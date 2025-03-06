53b0efd97e9b3d13f688e4aae0569f3cebb667cf
Johntadeo Liscano <jliscano@paterostechnologicalcollege.edu.ph>
2025-03-07 00:48:39 +0800

    Create Liscano.java
    Added Liscano.java with age calculation and updated week5.java

---
+++ Liscano.java
@@ -0,0 +1,7 @@
+public class Liscano {
+    public static void display() {
+        int age = 20; // Replace with user input if necessary
+        System.out.println("Hello! This is Liscano contributing to the project.");
+        System.out.println("My current age is: " + 20);
+        System.out.println("Next year, I will be: " + (age + (21));
+  
