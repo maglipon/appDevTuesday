f5be2fd1b771d47139b14f78778e5172ea17e624
MarcusPanganiban <mppanganiban@paterostechnologicalcollege.edu.ph>
2025-03-08 15:19:30 +0800

    Update week5.java

--- week5.java
+++ week5.java
@@ -31,6 +31,7 @@ public class Week5 {
         Miranda.display();
         Pajanustan.display();
         Panis.display();
+        Panganiban.dispaly();
         Pascualado.display();
         Pianar.display();
         Regoroso.display();
