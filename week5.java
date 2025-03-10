c21587c49411bc88419735012d849f7a55acdbd8
shanemico <sjacal@paterostechnologicalcollege.edu.ph>
2025-03-11 02:10:46 +0800

    Added ACAL.java with age calculation and updated week5.java

--- week5.java
+++ week5.java
@@ -1,49 +1,8 @@
-public class Week5 {
+public class week5 {
     public static void main(String[] args) {
         System.out.println("Collaborative Java Project\n");
 
-        // Call each student's method in alphabetical order
-        AgpoonGregpsalms.display();
-        AgpoonGregsamuel.display();
-        Alabado.display();
-        Amar.display();
-        Arevalo.display();
-        Artiaga.display();
-        Balais.display();
-        Banaag.display();
-        Bendoy.display();
-        Berte.display();
-        Borja.display();
-        Credo.display();
-        Costanilla.display();
-        DeJesus.display();
-        Dichoso.display();
-        Dizon.display();
-        Echon.display();
-        Eugenio.display();
-        Garcia.display();
-        Gabronino.display();
-        Jesalva.display();
-        Lamac.display();
-        Mago.display();
-        Mangui.display();
-        Mata.display();
-        Miranda.display();
-        Pajanustan.display();
-        Panis.display();
-        Pascualado.display();
-        Pianar.display();
-        Regoroso.display();
-        Respondo.display();
-        Ringor.display();
-        Saulong.display();
-        Sayas.display();
-        Serrano.display();
-        Tabon.display();
-        Talidong.display();
-        Tapia.display();
-        Tado.display();
-        Villaos.display();
-        Villon.display();
+        // Call each student's method
+        ACAL.display(); // Add your method call here
     }
 }
