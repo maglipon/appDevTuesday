77c711cc9f37b65fc87144acb0762fdf56faeedd
harryypottt <mmbaldo@paterostechnologicalcollege.edu.ph>
2025-03-11 20:35:37 +0800

    Update GroupWork.java

--- GroupWork.java
+++ GroupWork.java
@@ -10,7 +10,7 @@ public class GroupWork   {
        
         // Calling methods from Calculator.java
         System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
-        
+        System.out.println("Max: " + max(num1, num2)); // Group 8's method
 
        System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
 
