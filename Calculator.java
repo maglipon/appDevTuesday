5acd3402afe016eb72cb5d29bdd08f3c4b9edbca
BSIT2CBerte <jmberte@paterostechnologicalcollege.edu.ph>
2025-03-11 19:17:10 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -6,13 +6,15 @@ Group 3 : Product ( Echon, Gabronino, Acal, Villon, Ladera, Emnil, Debuton, Manu
 // Calculator.java
 public class Calculator {
    
-
-    // Group 1: Addition (Members: Panis,Alabado,Bendoy,Dichoso,Mangui,Sayas,Serrano,Tabilog,Talidong,Villaos)
-
     // Group 1: Addition (Members: Panis, Alabado, Bendoy, Dichoso, Mangui, Sayas, Serrano, Tabilog, Talidong, Villaos)
-
     public static int add(int a, int b) {
         return a + b;
     }
+ 
+ //Group 4: Division (Members: Balais, Tabon, Amar, Pajanustan, Garcia, Banaag, Ringor, Berte)
+	public static int divide(int a, int b){
+		return a / b;
+	}
+
 
 
