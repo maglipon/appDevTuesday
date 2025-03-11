3bd15801d21549c3924f7a36b57430422dfb902b
adpajanustan <adpajanustan@paterostechnologicalcollege.edu.ph>
2025-03-11 19:29:50 +0800

    added group 4 division method

--- GroupWork.java
+++ GroupWork.java
@@ -1,38 +1,21 @@
-
 import java.util.Scanner;
-public class Groupwork { 
 
+public class GroupWork   {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-       System.out.println("Enter first number: ");
-        int num1 = scan.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num1 = scanner.nextInt();
         System.out.println("Enter second number: ");
-        int num2 = scan.nextInt();
+        int num2 = scanner.nextInt();
        
         // Calling methods from Calculator.java
-
         System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
-	System.out.println("The square of " + num1 + " is: " + Calculator.square(num1)); // Group 6's method
- 
+        
+        
+        System.out.println("Quotient: " + Calculator.divide(num1, num2));  // Group 4 Method
+
        System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
 
-        System.out.println("Product  " + (num1 * num2));  // Group 3's method
-        
-
-
-
-// Calculator.java
-public class Calculator {
-   
-    //Group 4: Division (Members: Balais, Tabon, Amar, Pajanustan, Garcia, Banaag, Ringor, Berte)
-	public static int divide(int a, int b) {
-		return a / b;
-	}    
-    public static boolean isEqual(int a, int b) {
-        return a == b;
-
+        scanner.close();
     }
-
-    // Other groups will add their methods here
 }
