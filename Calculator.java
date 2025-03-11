4fd1f8e1b6f4bda3d931dcc5f4e834635c7dc23a
gabroninolg <lngabronino@gmail.com>
2025-03-11 23:21:00 +0800

    Added Calculator.java updated Groupwork.java

--- Calculator.java
+++ Calculator.java
@@ -1,23 +1,3 @@
-// Calculator.java
-public class Calculator {
-   
-    // Group 1: Addition (Members: John, Sarah, Alex)
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
-
-    // Other groups will add their methods here
-
-   // Group 5: Modulus (Members: Miranda, Abagat, Garnica, Cababao, De Jesus, Latagan, Sudicta, Borja, Fernandez)
-    modulus(int a, int b) {
-   return a % b;
-      }
-
-    // Group 10: Check if numbers are equal (Members: Jake, Emily, Ryan)
-    public static boolean isEqual(int a, int b) {
-        return a == b;
-    }
-
-    
-}
+Group 3 : Product ( Echon, Gabronino, Acal, Villon, Ladera, Emnil, Debuton, Manuel, Tado, Carino)
+ // public static int multiply (int a * int b) {
+  //      return a * b;
