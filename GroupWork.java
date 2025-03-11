de8aa6ed5d9477a51101d3a64b2263dd0c7f3cbd
Ryan Echon <rpechon@paterostechnologicalcollege.edu.ph>
2025-03-11 19:06:50 +0800

    Added Calculator.java updated GroupWork.java

--- GroupWork.java
+++ GroupWork.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
+public class Groupwork { 
 
-public class GroupWork   {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first number: ");
-        int num1 = scanner.nextInt();
+        Scanner scan = new Scanner(System.in);
+        
+       System.out.println("Enter first number: ");
+        int num1 = scan.nextInt();
         System.out.println("Enter second number: ");
-        int num2 = scanner.nextInt();
+        int num2 = scan.nextInt();
        
         // Calling methods from Calculator.java
-        System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
+        System.out.println("Product  " + (num1 * num2));  // Group 3's method
         
 
-       System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
-
-        scanner.close();
     }
 }
