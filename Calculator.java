f983d796be7852c8db63019cce5bb4f37aa5ab84
JohnMarkBalais <jpbalais@paterostechnologicalcollege.edu.ph>
2025-03-11 19:13:38 +0800

    Added Group 4: Division Method

--- Calculator.java
+++ Calculator.java
@@ -8,6 +8,11 @@ public class Calculator {
 
 
     // Other groups will add their methods here
+	
+	//Group 4: Division (Members: Balais, Tabon, Amar, Pajanustan, Garcia, Banaag, Ringor, Berte)
+	public static int divide(int a, int b){
+		return a / b;
+	}
 
     // Group 10: Check if numbers are equal (Members: Jake, Emily, Ryan)
     public static boolean isEqual(int a, int b) {
