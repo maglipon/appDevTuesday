7e33d56c5a3bdaa16b1695db07a548b21276bc34
Ralph Jastine Villon <rvvillon@paterostechnologicalcollege.edu.ph>
2025-03-11 21:23:52 +0800

    Added calculator.java update Groupwork.java

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,9 @@ public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
-
+    //Group 3 : Product ( Echon, Gabronino, Acal, Villon, Ladera, Emnil, Debuton, Manuel, Tado, Carino)
+ public static int multiply (int a * int b) {
+   return a * b;
     //Group 4: Division (Members: Balais, Tabon, Amar, Pajanustan, Garcia, Banaag, Ringor, Berte)
      public static int divide(int a, int b){
 	return a / b;
