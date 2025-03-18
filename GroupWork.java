063d819edf84fec5a87deebe3882b775e8f6455a
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-03-18 16:10:08 +0800

    Update GroupWork.java FINAL UPDATE

--- GroupWork.java
+++ GroupWork.java
@@ -7,29 +7,14 @@ public class GroupWork   {
         int num1 = scanner.nextInt();
         System.out.println("Enter second number: ");
         int num2 = scanner.nextInt();
-       
-        // Calling methods from Calculator.java
+             // Calling methods from Calculator.java
         System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
-
         System.out.println("Max: " + max(num1, num2)); // Group 8's method
-
 	System.out.println("Product: " + Calculator.multiply(num1, num2)); // Group 3's method
-
         System.out.println("Cube: " + Calculator.cube(num1));  // Group 7's method
-        
-
-
-
-        System.out.println("The square of " + num1 + " is: " + Calculator.square(num1)); // Group 6's method
+                System.out.println("The square of " + num1 + " is: " + Calculator.square(num1)); // Group 6's method
         System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
-
-        scanner.close();
-    }
-}
-
-
         System.out.println("Difference: " + Calculator.subtract(num1, num2));    // Group 2's method
-
         System.out.println("Quotient: " + Calculator.divide(num1, num2));  // Group 4 Method
         System.out.println("Remainder: " + Calculator.remainder(num1, num2));  // Group 5's Method
         System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
